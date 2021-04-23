fcb89983e828ad074249e3938b890f5016933c33
Thaysa Nery <thaysanery294@gmail.com>
2021-04-23 16:07:30 -0300

    Dia 05 versão 1

--- podcastrnext/src/pages/_app.tsx
+++ podcastrnext/src/pages/_app.tsx
@@ -2,27 +2,12 @@ import { Header } from "../components/Header";
 import '../styles/global.scss'
 import styles from'../styles/app.module.scss'
 import { Player } from "../components/Player";
-import { PlayerContext } from "../contexts/PlayerContext";
 import { useState } from "react";
 
 
-function MyApp({ Component, pageProps }) {
-const [episodeList, setEpisodeList] = useState([]);
-const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-const [isPlaying, setIsPlaying] = useState(false);
-function togglePlay(){
-  setIsPlaying(!isPlaying);
-}
-function play(episode){
-  setEpisodeList([episode]);
-  setCurrentEpisodeIndex(0);
-  setIsPlaying(true);
-}
-function setPlayingState(state: boolean){
-setIsPlaying(state);
-}
-  return(
-    <PlayerContext.Provider value={{episodeList, currentEpisodeIndex,play, isPlaying, togglePlay, setPlayingState}}>
+function MyApp({Component, pageProps}) {
+  
+   return(
     <div className={styles.Wrapper}>
       <main>
          <Header />
@@ -30,7 +15,6 @@ setIsPlaying(state);
       </main>
      <Player/>
     </div>
-    </PlayerContext.Provider>
      )
 }
 
